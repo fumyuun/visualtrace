1dd20e03c6e9ced801d7d7854a7d69aa19932405
Koray Yanik <fumyuun@gmail.com>
2015-02-02 15:57:40 +0100

    Tokenize input lines by splitting on spaces

--- scripts/parser/parser.js
+++ scripts/parser/parser.js
@@ -1,4 +1,4 @@
-define(['util'], function() {
+define(['util'], function(util) {
     'use strict';
 
 	var Parser = function () {
@@ -11,7 +11,22 @@ define(['util'], function() {
 
 	Parser.prototype.prepare = function (input) {
 		input = input.split('\n');
-		console.log(input);
+
+		util.forEach(input, function (line, l) {
+			var i;
+			line = line.split(' ');
+			i = 0;
+			while(i < line.length) {
+				if (line[i] == '') {
+					line.splice(i, 1);
+				} else {
+					++i;
+				}
+			}
+			input[l] = line;
+		});
+
+		return input;
 	}
 
 	return Parser;
