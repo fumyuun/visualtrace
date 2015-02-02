f3b9796cb6160e2b60f563e5771e6485ca63c63c
Koray Yanik <fumyuun@gmail.com>
2015-02-02 16:02:28 +0100

    Parse OSX lines into a graph like structure

--- scripts/parser/parser_osx.js
+++ scripts/parser/parser_osx.js
@@ -2,10 +2,45 @@ define(['parser/parser', 'util'], function(Parser, util) {
     'use strict';
 
 	var OsxParser = util.createClass(Parser, function () {
+		this.data = {};
 	});
 
 	OsxParser.prototype.parse = function (input) {
-		this.prepare(input);
+		var hop = 0,
+			me = this;
+
+		input = this.prepare(input);
+
+		this.data.nodes = [];
+		this.data.links = [];
+
+		util.forEach(input, function (tokens) {
+			var node = {},
+				token = 0;
+			if (tokens[token] == 'traceroute' || tokens[token] == 'traceroute:') {
+				// Diagnostics, ignore
+			} else {
+				// Either next hop line or alternative route to same hop
+				if (parseInt(tokens[token]) == hop + 1) {
+					hop++;
+					token++;
+				}
+				node.depth = hop;
+				node.hostname = tokens[token++];
+				node.ip = tokens[token++];
+				node.latencies = [];
+				for (; token < tokens.length; token+=2) {
+					node.latencies.push(tokens[token]);
+				}
+				me.data.nodes.push(node);
+				if (hop > 1) {
+					me.data.links.push({source: (hop-1), target: hop});
+					me.data.links.push({source: hop, target: (hop-1)});
+				}
+			}
+		});
+
+		console.log(this.data);
 
 		return {};
 	}
