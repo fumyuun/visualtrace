705a6d875235af9c12f634ccdcc2f45196b0d3e2
Koray Yanik <fumyuun@gmail.com>
2015-02-05 14:36:00 +0100

    Handle cases where same ping returns different hostname by restarting the parser on that line

--- scripts/parser/parser_unix.js
+++ scripts/parser/parser_unix.js
@@ -17,26 +17,38 @@ define(['parser/parser', 'util'], function(Parser, util) {
 
 		util.forEach(input, function (tokens) {
 			var node = {},
-				token = 0;
+				token = 0,
+				parseNextNode = function () {
+					node = {};
+					if (parseInt(tokens[token]) == hop + 1) {
+						hop++;
+						token++;
+					}
+					node.group = 0;
+					node.depth = hop;
+					node.hostname = tokens[token++];
+					node.ip = tokens[token++];
+					node.name = node.hostname + node.ip;
+					node.latencies = [];
+					for (; token < tokens.length; token++) {
+						if (tokens[token] != '*' && tokens[token] != 'ms') {
+							if (isNaN(parseFloat(tokens[token]))) {
+								// Problematic line where pings to same host respond with different
+								// names. Restart parsing on the same line.
+								me.data.nodes.push(node);
+								parseNextNode();
+								return;
+							}
+							node.latencies.push(parseFloat(tokens[token]));
+						}
+					}
+
+				};
 			if (tokens[token] == 'traceroute' || tokens[token] == 'traceroute:') {
 				// Diagnostics, ignore
 			} else {
 				// Either next hop line or alternative route to same hop
-				if (parseInt(tokens[token]) == hop + 1) {
-					hop++;
-					token++;
-				}
-				node.group = 0;
-				node.depth = hop;
-				node.hostname = tokens[token++];
-				node.ip = tokens[token++];
-				node.name = node.hostname + node.ip;
-				node.latencies = [];
-				for (; token < tokens.length; token+=2) {
-					if (tokens[token] != '*') {
-						node.latencies.push(parseFloat(tokens[token]));
-					}
-				}
+				parseNextNode();
 				me.data.nodes.push(node);
 			}
 		});
